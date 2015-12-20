cab80b306db77550c52691705dfc49b7dbf79df8
CarlosDiogo01 <carlos.sa01@gmail.com>
2015-12-20 19:28:54 +0000

    Final Work | Cleaned Repo

---
+++ PCP_-_#2_MPI_Sparse_Matrix/Code/src/tp2_mpi.c
@@ -0,0 +1,241 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "mpi.h"
+
+
+/********************
+Desenvolvido por: 
+Carlos Sá- A59905
+Ana Sousa - A69855
+*********************/
+
+
+double** geraMatriz (int nLinhas, int nCols, int nnz) {
+	int i, j, x, y, aux, cont;
+	double **mat = (double **) calloc (nLinhas, sizeof(double*));
+	double **coo = (double **) calloc (nLinhas, sizeof(double*));
+	double valRand;
+
+	for (i=0; i<nLinhas; i++) {
+		mat[i] = (double*) calloc (nCols, sizeof(double));
+	}
+
+	srand(1);
+	for (i=0; i<nnz; i += aux) {
+		x = rand() % nLinhas;
+		y = rand() % nCols;
+		if (mat[x][y] == 0.0) {
+			while ((valRand = (rand() % 50) * 0.25) == 0) {;}
+			mat[x][y] = valRand; 
+			aux = 1;
+		} else {
+			aux = 0;
+		}
+	}
+
+	for (i=0; i<nLinhas; i++) {
+		cont = 0;
+
+		for(j=0; j<nCols; j++) { if (mat[i][j] != 0.0) {cont++;} }
+		if (cont>0) {
+			coo[i] = (double*) calloc ((cont*2)+1, sizeof(double));
+			coo[i][0] = cont * 2;
+			aux = 1;
+			for (j=0; j<nCols; j++) {
+				if (mat[i][j] != 0.0) {
+					coo[i][aux] = j;
+					coo[i][aux+1] = mat[i][j];
+					aux += 2;
+
+				}
+			}
+		} else {
+			coo[i] = (double*) calloc (1, sizeof(double));
+			coo[i][0] = 0;
+		}
+		free(mat[i]);
+	}
+	free(mat);
+	
+	return coo;
+}
+
+double* geraVetor (int nCols) {
+	int i;
+	double *vect;
+
+	vect = (double*) calloc (nCols, sizeof(double));
+	srand(1);
+	for(i=0; i<nCols; i++) {
+		vect[i] = (rand() % 20) * 0.25;
+	}
+	
+	return vect;
+}
+
+void forceLoadRam(){
+	int i;
+  	double forceloadram [30000000];
+  	for (i = 0; i < 30000000; ++i)
+    	forceloadram[i] = i;
+}
+
+int main(int argc, char *argv[]) {		
+	int nLinhas, nCols, nLinhasVect, i, j, incr, maxElem;
+	double startTime, endTime, startAux, endAux, tbcast, tcomp, trecv, tsend, wallTime, n;
+	double **coo, *vect, *result, *linha, res;
+	int MASTER = 0;
+
+	/* MPI */
+	MPI_Status status;	
+	int idProc;		/* ID de cada processo */
+	int tag;
+	int totalProcs;		/* Numero total de processos */
+	int procDest;		/* Processo que vai tratar a linha corrente */
+	int acabou; 		/* Para terminar a execucao do processo */
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &idProc);
+	MPI_Comm_size(MPI_COMM_WORLD, &totalProcs);
+
+	nLinhas = atoi(argv[1]);
+	nCols = atoi(argv[2]);
+	nLinhasVect = atoi(argv[3]);
+	acabou = nLinhas + 1;
+	incr = (int)(nLinhas/(totalProcs-1));
+	maxElem = (nCols*2) + 1;
+	tcomp = 0; tsend = 0; trecv = 0; tbcast = 0;
+	wallTime = 0; startTime = endTime = 0;
+
+	linha = (double*) calloc (maxElem, sizeof(double));
+	result = (double*) calloc (nLinhas, sizeof(double));
+	vect = (double*) calloc (nLinhasVect, sizeof(double));
+
+	if (idProc == MASTER){
+		/* Preenchimento Matriz e Vetor */
+		unsigned int nnz = (nLinhas * nCols) * 0.25;
+		coo = geraMatriz(nLinhas, nCols, nnz);
+		vect = geraVetor(nLinhasVect);
+	}
+
+	/* Enviar o vetor gerado no MASTER aos outros processos */
+	startAux = MPI_Wtime();
+	MPI_Bcast(vect, nLinhasVect, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+	endAux = MPI_Wtime();
+	tbcast = endAux - startAux;
+	forceLoadRam();
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if (idProc == MASTER){
+	//	forceLoadRam();
+		startTime = MPI_Wtime(); //walltime
+		startAux = MPI_Wtime(); //tsend
+		procDest = 1;
+		for (i=0; (i<nLinhas) && (procDest<totalProcs); i+= incr){
+			n = i+incr;
+			for (j=i; j<n; j++) {
+				tag = j;
+				MPI_Send(coo[j], (int)(coo[j][0] + 1), MPI_DOUBLE, procDest, tag, MPI_COMM_WORLD);
+			}
+			procDest++;
+		}
+		endAux = MPI_Wtime(); 
+		tsend += (endAux - startAux);
+
+		//Computacao das linhas que sobraram
+		startAux = MPI_Wtime(); //tcomp
+		while(i < nLinhas){
+			res = 0.0;
+			n = (int)coo[i][0];
+			for(j=1; j<n; j+=2){
+				res += coo[i][j+1] * vect[(int)coo[i][j]];
+			}
+			result[i] = res;
+			i++;
+		}
+		endAux = MPI_Wtime(); 
+		tcomp = endAux - startAux;
+
+		endTime = MPI_Wtime();
+		wallTime = endTime - startTime;
+		
+		//printf("Wall time proc %d: %.12f\n", idProc, wallTime*100);
+	}
+
+	//Caso nao seja o MASTER
+	else{
+		startTime = MPI_Wtime();
+		for(i=0; i<incr; i++){
+			startAux = MPI_Wtime();
+			MPI_Recv(linha, maxElem, MPI_DOUBLE, MASTER, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			tag = status.MPI_TAG;
+			endAux = MPI_Wtime();
+			trecv += (endAux - startAux);
+			
+			startAux = MPI_Wtime();
+			res = 0.0;
+			n = (int)linha[0];
+			for(j=1; j<=n; j+=2){
+				res += linha[j+1] * vect[(int)linha[j]];
+			}
+			result[tag] = res;
+			endAux = MPI_Wtime();
+			tcomp += (endAux - startAux);
+		}
+		endTime = MPI_Wtime();
+		wallTime = endTime - startTime;
+	}
+	
+	MPI_Barrier(MPI_COMM_WORLD);
+	double result_global[nLinhas], wallTime_global, trecv_global, tcomp_global;
+	
+	MPI_Reduce(result, result_global, nLinhas, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
+	MPI_Reduce(&wallTime, &wallTime_global, 1, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
+	MPI_Reduce(&trecv, &trecv_global, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+	MPI_Reduce(&tcomp, &tcomp_global, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if(idProc == MASTER) {
+		/* Visualizacao dos resultados */
+		/*printf("---------Matriz formato COO---------\n");
+		for(i=0; i<nLinhas; i++){
+			if (coo[i]) {
+				n = coo[i][0];
+				for(j=1; j<=n; j+=2) {
+					printf("%f %f %f\n", (double)i, (double)coo[i][j], (double)coo[i][j+1]);
+				}
+			}
+		}
+		
+		printf("\n---------Vetor---------\n");
+		for(i=0; i<nLinhasVect; i++) {
+			printf("---%f ", vect[i]);
+		}
+		printf("\n");
+
+		printf("-----------Resultado Multiplicacao---------\n");
+		for(i=0; i<nLinhas; i++)
+			printf("---%f ", result_global[i]);
+		printf("\n\n");
+		
+		*/
+		printf("%.12f, %.12f, %.12f, %.12f, %.12f\n", tbcast*1000, tsend*1000, trecv_global*1000, tcomp_global*1000, wallTime_global*1000);
+
+		/* Libertar a memoria alocada */
+		for(i=0; i<nLinhas; i++) {
+			free(coo[i]);
+		}
+		free(coo);
+	}
+
+	free(linha);
+	free(vect);
+	free(result);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Finalize();
+	return 0;
+}
+
---
+++ PCP_-_#2_MPI_Sparse_Matrix/Final/PCP_TP2_A59905_A69855/Code_PCP_TP2_A59905_A59855/TP2_SpMV_MPI/build/tp2_mpi.d
@@ -0,0 +1,18 @@
+tp2_mpi.o: src/tp2_mpi.c /usr/include/stdio.h /usr/include/features.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /share/apps/gcc/4.9.3/lib/gcc/x86_64-unknown-linux-gnu/4.9.3/include/stddef.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /share/apps/gcc/4.9.3/lib/gcc/x86_64-unknown-linux-gnu/4.9.3/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h /usr/include/string.h \
+ /usr/include/bits/string.h /usr/include/bits/string2.h \
+ /usr/include/endian.h /usr/include/bits/endian.h /usr/include/stdlib.h \
+ /usr/include/math.h /usr/include/bits/huge_val.h \
+ /usr/include/bits/huge_valf.h /usr/include/bits/huge_vall.h \
+ /usr/include/bits/inf.h /usr/include/bits/nan.h \
+ /usr/include/bits/mathdef.h /usr/include/bits/mathcalls.h \
+ /usr/include/bits/mathinline.h \
+ /share/apps/openmpi/1.8.4/gnu/eth/include/mpi.h \
+ /share/apps/openmpi/1.8.4/gnu/eth/include/mpi_portable_platform.h
